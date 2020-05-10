0fe9df975a8d18395417a6633144e60283538a8a
federicotallis <Federico.tallis@gmail.com>
2020-05-09 23:10:16 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,14 +22,14 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 var map = new mapboxgl.Map({
   container: 'map',
   style: MAPBOX_STYLE,
-  center: [-74.005, 40.714],
+  center: [-71.0659051,42.3579113],
   zoom: 13,
   maxZoom: 22,
   minZoom: 13,
-  maxBounds: [
-    [-74.33, 40.41], // Southwest coordinates
-    [-73.63, 40.98] // Northeast coordinates
-  ],
+ //maxBounds: [
+  // [-74.33, 40.41], // Southwest coordinates
+   // [-73.63, 40.98] // Northeast coordinates
+  //],
   hash: true
 });
 
