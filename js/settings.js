284826161adbd878e7091eb79c54f467eff0dcf1
federicotallis <Federico.tallis@gmail.com>
2020-05-16 14:09:25 -0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoiZmVkZXJpY290YWxsaXN2aGIiLCJhIjoiY2s5anp0d3h5MXBwaTNtbndnZjJiMHQyeiJ9.EkcnAumdNONuvCxlASioDw'
-var MAPBOX_STYLE = 'mapbox://styles/federicotallisvhb/ck9zyok013tjk1imte3akocfp'
+var MAPBOX_STYLE = 'mapbox://styles/federicotallisvhb/cka9xi3hg3wlt1io8ppz9jbqq'
 var SIDEWALKS_TILESET = 'mapbox://federicotallisvhb.2anopi8n'
 var SIDEWALKS_LAYER = 'Buenos_Aires_Sidewalk_Widths-97vmxa'
 var UNITS = 'm' // change to 'm' for meters
