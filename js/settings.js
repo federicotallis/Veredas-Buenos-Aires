13645663842a12079817ff9234baeac8906e6d41
federicotallis <Federico.tallis@gmail.com>
2020-05-16 10:50:50 -0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,8 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoiZmVkZXJpY290YWxsaXN2aGIiLCJhIjoiY2s5anp0d3h5MXBwaTNtbndnZjJiMHQyeiJ9.EkcnAumdNONuvCxlASioDw'
 var MAPBOX_STYLE = 'mapbox://styles/federicotallisvhb/ck9zyok013tjk1imte3akocfp'
-var SIDEWALKS_TILESET = 'mapbox://federicotallisvhb.a3sixxsl'
-var SIDEWALKS_LAYER = 'output-2k8uxk'
-var UNITS = 'ft' // change to 'm' for meters
+var SIDEWALKS_TILESET = 'federicotallisvhb.2anopi8n'
+var SIDEWALKS_LAYER = 'Buenos_Aires_Sidewalk_Widths-97vmxa'
+var UNITS = 'm' // change to 'm' for meters
 var PRECISION = 0.1 // the number of decimal places
 var GROUPS = [
   {
@@ -11,32 +11,32 @@ var GROUPS = [
     "color": "#FF0049"
   },
   {
-    "value": 6,
+    "value": 2,
     "rating": "very difficult",
     "color": "#FF461E"
   },
   {
-    "value": 9,
+    "value": 3,
     "rating": "difficult",
     "color": "#FF9300"
   },
   {
-    "value": 12,
+    "value": 4,
     "rating": "somewhat difficult",
     "color": "#DAD130"
   },
   {
-    "value": 15,
+    "value": 5,
     "rating": "somewhat easy",
     "color": "#00ff5a"
   },
   {
-    "value": 18,
+    "value": 6,
     "rating": "easy",
     "color": "#00FFC4"
   },
   {
-    "value": 21,
+    "value": 7,
     "rating": "very easy",
     "color": "#00D2FF"
   },
