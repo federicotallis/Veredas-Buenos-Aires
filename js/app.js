80d5412712a9a2c8a09051ef09ad451ed71568c0
federicotallis <Federico.tallis@gmail.com>
2020-05-16 11:35:41 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -35,10 +35,6 @@ var map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.NavigationControl());
 
-map.addControl(new MapboxGeocoder({accessToken: MAPBOX_TOKEN,
-                                   mapboxgl: mapboxgl})
-
-
 map.on('load', function() {
   // Insert the layer beneath any symbol layer.
   var layers = map.getStyle().layers;
